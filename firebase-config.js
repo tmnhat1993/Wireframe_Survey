50e45417bc0176f61b20788d1854421b383fab8f
nhat <tmnhat1993@gmail.com>
2026-06-01 20:48:44 +0700

    Enable Firebase project config

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 window.SURVEY_FIREBASE_CONFIG = {
-  enabled: false,
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  enabled: true,
+  apiKey: "AIzaSyDS77mieTAlnhW9oHya7piimHroakWlf4U",
+  authDomain: "surveydata-fd3b6.firebaseapp.com",
+  projectId: "surveydata-fd3b6",
+  storageBucket: "surveydata-fd3b6.firebasestorage.app",
+  messagingSenderId: "275839584383",
+  appId: "1:275839584383:web:df85d2ba05d8c3ac79174a"
 };
 
 window.SURVEY_ADMIN_FALLBACK = {
